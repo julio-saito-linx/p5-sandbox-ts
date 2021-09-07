cd3dec903b1825510c8c2c10e8eb753f1b9b17a2
Julio Saito <saitodisse@gmail.com>
2021-09-07 01:01:15 -0300

    first elipse

--- src/sketch.ts
+++ src/sketch.ts
@@ -1,21 +1,30 @@
-import p5 from 'p5'
+import p5 from "p5";
 
 /**
  * @param {p5} p
  */
 export const sketch = (p: p5) => {
+  const canvasWidth = p.windowWidth - 40;
+  const canvasHeight = p.windowHeight - 20;
   p.setup = () => {
-    // Define your initial environment props & other stuff here
-  }
+    p.createCanvas(canvasWidth, canvasHeight);
+    p.frameRate(30);
+  };
 
   p.draw = () => {
-    // Define render logic for your sketch here
-  }
+    p.background(50);
+
+    const numbers = [0, 1, 2, 3, 4, 5, 6].reverse();
+    for (let i = 0; i < numbers.length; i++) {
+      const element = numbers[i];
+      p.ellipse(canvasWidth / 2, canvasHeight / 2, 50 * element, 50 * element);
+    }
+  };
 
   p.keyPressed = () => {
     // Export sketch's canvas to file
     if (p.keyCode === 80) {
-      p.saveCanvas('sketch', 'png')
+      p.saveCanvas("sketch", "png");
     }
-  }
-}
+  };
+};
