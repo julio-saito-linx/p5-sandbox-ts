90d1090eb110b3cc1ddecc3c155ec8f683e80bda
Julio Makdisse Saito <julio@latife.com.br>
2021-09-12 01:14:45 -0300

    create ball on mouse click, max 10

--- src/experiments/simpleGravity.ts
+++ src/experiments/simpleGravity.ts
@@ -1,28 +1,17 @@
 import p5 from 'p5'
 import { Ball } from '../objects/ball'
 import { getCanvasWidth, getCanvasHeight } from '../utils'
-import { createBalls } from '../objects/createBalls'
 
 /**
  * @param {p5} p
  */
 export const simpleGravity = (p: p5) => {
-  let ball: Ball
+  let balls: Ball[] = []
+  const MAX_BALLS: number = 10
 
   p.setup = () => {
     const canvas = p.createCanvas(getCanvasWidth(p), getCanvasHeight(p))
     canvas.parent('sketch-holder')
-    // p.frameRate(60)
-
-    ball = new Ball({
-      _p: p,
-      position: p.createVector(getCanvasWidth(p) / 2, 0),
-      velocity: p.createVector(0, 0),
-      acceleration: p.createVector(0, 0),
-      raid: 30,
-      mass: 1,
-      defaultBgColor: 30,
-    })
   }
 
   const gravity = p.createVector(0, 0.6)
@@ -33,38 +22,55 @@ export const simpleGravity = (p: p5) => {
 
     p.fill(255, 0, 0)
 
-    ball.applyForce(gravity)
-    ball.applyForce(wind)
+    for (let i = 0; i < balls.length; i += 1) {
+      const ball = balls[i]
 
-    if (ball.position.y > p.height - ball.raid / 2) {
-      // A little dampening when hitting the bottom
-      ball.velocity.y *= -0.7
-      ball.position.y = p.height - ball.raid / 2
-    }
-    if (ball.position.x > p.width - ball.raid / 2) {
-      // A little dampening when hitting the bottom
-      ball.velocity.x *= -0.4
-      ball.position.x = p.width - ball.raid / 2
-      wind.rotate(180)
-    }
-    if (ball.position.x < 0 + ball.raid / 2) {
-      // A little dampening when hitting the bottom
-      ball.velocity.x *= -0.4
-      ball.position.x = 0 + ball.raid / 2
-      wind.rotate(180)
-    }
+      ball.applyForce(gravity)
+      ball.applyForce(wind)
 
-    ball.changeColorByVelocity()
-    ball.paint()
+      if (ball.position.y > p.height - ball.raid / 2) {
+        // A little dampening when hitting the bottom
+        ball.velocity.y *= -1
+        ball.position.y = p.height - ball.raid / 2
+      }
+      if (ball.position.x > p.width - ball.raid / 2) {
+        // A little dampening when hitting the bottom
+        ball.velocity.x *= -1
+        ball.position.x = p.width - ball.raid / 2
+        wind.rotate(180)
+      }
+      if (ball.position.x < 0 + ball.raid / 2) {
+        // A little dampening when hitting the bottom
+        ball.velocity.x *= -1
+        ball.position.x = 0 + ball.raid / 2
+        wind.rotate(180)
+      }
+
+      ball.changeColorByVelocity()
+      ball.paint()
+    }
   }
 
   p.windowResized = () => {
     p.resizeCanvas(getCanvasWidth(p), getCanvasHeight(p))
   }
 
-  p.mousePressed = () => {
-    ball.position.x = p.mouseX
-    ball.position.y = p.mouseY
-    ball.velocity.y = 0
+  p.mousePressed = (ev) => {
+    console.log('--  ev: ', ev)
+    const ball = new Ball({
+      _p: p,
+      position: p.createVector(p.mouseX, p.mouseY),
+      velocity: p.createVector(0, 0),
+      acceleration: p.createVector(0, 0),
+      raid: 30,
+      mass: 1,
+      defaultBgColor: 30,
+    })
+
+    if (balls.length > MAX_BALLS) {
+      balls.shift()
+    }
+
+    balls.push(ball)
   }
 }
