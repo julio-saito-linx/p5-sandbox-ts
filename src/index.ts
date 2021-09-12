d2bb0f205e982d95a313b4b9ef13e5d301f8718b
Julio Makdisse Saito <julio@latife.com.br>
2021-09-12 01:51:54 -0300

    fix routes

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,17 @@ import { ballsInsideBalls } from './experiments/ballsInsideBalls'
 import { simpleGravity } from './experiments/simpleGravity'
 
 // show list of experiments
-if (document.location.pathname === '/' || document.location.pathname === '') {
+if (
+  document.location.href === `${document.location.origin}/p5-sandbox-ts` ||
+  document.location.href === `${document.location.origin}/`
+) {
   document.querySelector<HTMLDivElement>('.experiments').style.display = 'flex'
 }
 
 // experiments:
-if (document.location.pathname === '/ballsInsideBalls') {
+if (document.location.href === `${document.location.origin}/p5-sandbox-ts/ballsInsideBalls`) {
   new p5(ballsInsideBalls)
 }
-if (document.location.pathname === '/simpleGravity') {
+if (document.location.href === `${document.location.origin}/p5-sandbox-ts/simpleGravity`) {
   new p5(simpleGravity)
 }
